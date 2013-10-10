27a6c77f9476fcd7d50a15d5b45a75024efc14ad
rsaez <rsaez@gatech.edu>
2013-10-09 22:33:22 -0400

    defualt
    initial root directory installed by Ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5f5ed592b648bcb52bb91613a4810bf03c02569d402d72c0e10917334ffec0136bbf059812acc257c3c2827b31ea3c7a5152a465a5356c93bd2297c75b50f892'
